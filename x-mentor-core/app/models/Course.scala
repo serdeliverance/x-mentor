31733b31820b6ecf75434d781127d47415488188
Nicolas <osorionicolas95@gmail.com>
2021-04-17 03:22:50 -0300

    removed unused imports

--- x-mentor-core/app/models/Course.scala
+++ x-mentor-core/app/models/Course.scala
@@ -1,7 +1,3 @@
 package models
 
-import io.circe.{Decoder, Encoder}
-import io.circe.generic.extras.semiauto._
-import models.json.CirceImplicits
-
 case class Course(id: Long, title: String, description: String, content: String, preview: String, topic: String, rating: Int)
