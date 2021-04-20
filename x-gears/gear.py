e8aacee86c4e2cf0b4d75d2eab7301e4e751b032
Sergio Cano <canosergio90@gmail.com>
2021-04-20 01:14:26 -0300

    gears - initial commit

---
+++ x-gears/gear.py
@@ -0,0 +1,14 @@
+import redisgears
+
+def xlog(*args):
+    redisgears.executeCommand('xadd', 'log', '*', 'text', ' '.join(map(str, args)))
+
+def toIA(x):
+	xlog('toIA: ', x)
+
+def toTimeSeries(x):
+	redisgears.executeCommand('set', 'foo', 'bar')
+
+gearsCtx('StreamReader').\
+    foreach(toTimeSeries).\
+    register('course-rated:0')
