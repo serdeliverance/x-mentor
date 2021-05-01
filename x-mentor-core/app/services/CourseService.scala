74fa35e9bd72bcb8b4d71abc03549a0f5aff6bb7
Nicolas <osorionicolas95@gmail.com>
2021-05-01 14:53:02 -0300

    refactor course creation

--- x-mentor-core/app/services/CourseService.scala
+++ x-mentor-core/app/services/CourseService.scala
@@ -1,7 +1,6 @@
 package services
 
 import akka.Done
-import akka.Done.done
 import cats.data.EitherT
 import constants.{COURSE_IDS_FILTER, COURSE_KEY, COURSE_LAST_ID_KEY, ITEMS_PER_PAGE, USERS_FILTER}
 import global.{ApplicationResult, ApplicationResultExtended}
@@ -40,20 +39,20 @@ class CourseService @Inject()(
     with ApplicationResultUtils
     with RedisJsonUtils {
 
-  def create(course: Course): ApplicationResult[Done] =
-    ApplicationResult {
+  def create(course: Course): ApplicationResult[Done] = {
       val redisInstance      = redisPool.getResource
       val currentIndex: Long = redisInstance.get(COURSE_LAST_ID_KEY).toLong + 1
       val key                = s"$COURSE_KEY$currentIndex"
       val updatedCourse      = course.copy(id = Some(currentIndex))
 
       logger.info(s"Storing course $currentIndex in Redis, increasing last id and adding to bloom filter")
-      // Insert into redisJSON
-      redisJsonRepository.set(key, CourseConverter.courseToMap(updatedCourse).asJava)
       redisInstance.incr(COURSE_LAST_ID_KEY)
-      // Insert into bloom filter
-      redisBloomRepository.add(COURSE_IDS_FILTER, currentIndex.toString)
-    }.map(_ => Right(done()))
+
+      for {
+        _ <- EitherT(redisJsonRepository.set(key, CourseConverter.courseToMap(updatedCourse).asJava))
+        _ <- EitherT(redisBloomRepository.add(COURSE_IDS_FILTER, currentIndex.toString))
+      } yield Done
+    }.value
 
   def enroll(courseId: Long, username: String): ApplicationResult[Done] = {
     logger.info(s"Enrolling user $username in course $courseId")
