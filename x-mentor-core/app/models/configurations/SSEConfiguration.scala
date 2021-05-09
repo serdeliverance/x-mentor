085f34fda612f0dabacb4d9b291bb8a68b618d90
Sergio Cano <canosergio90@gmail.com>
2021-05-08 22:13:18 -0300

    adding keep alive to sse

--- x-mentor-core/app/models/configurations/SSEConfiguration.scala
+++ x-mentor-core/app/models/configurations/SSEConfiguration.scala
@@ -2,6 +2,7 @@ package models.configurations
 
 import akka.NotUsed
 import akka.actor.ActorRef
+import akka.http.scaladsl.model.sse.ServerSentEvent
 import akka.stream.scaladsl.Source
 
 case class SSEConfiguration(notificationActor: ActorRef, sseSource: Source[String, NotUsed])
