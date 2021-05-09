25d10bf2166a9aa64bd35a2ec1091556d6447522
Sergio Cano <sergio.cano@redb.ee>
2021-05-09 13:48:39 -0300

    sse fix

--- x-mentor-core/app/controllers/NotificationController.scala
+++ x-mentor-core/app/controllers/NotificationController.scala
@@ -1,29 +1,32 @@
 package controllers
 
+import akka.Done
 import akka.actor.ActorSystem
-import models.configurations.SSEConfiguration
+import akka.stream.scaladsl.Source
+import akka.stream.{CompletionStrategy, OverflowStrategy}
 import play.api.Logging
 import play.api.http.ContentTypes
+import play.api.libs.EventSource
+import play.api.libs.EventSource.Event
 import play.api.mvc._
+import services.SseService
 
 import java.time.LocalDateTime
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.DurationInt
 
 @Singleton
 class NotificationController @Inject()(
     val controllerComponents: ControllerComponents,
-    sseConfiguration: SSEConfiguration
+    sseService: SseService
   )(implicit ec: ExecutionContext,
     system: ActorSystem)
     extends BaseController
     with Logging {
 
-  sealed trait NotificationMessage
-  case class CourseCreated(course: String, createdAt: LocalDateTime) extends NotificationMessage
-
   def subscribeToNotifications(): Action[AnyContent] = Action {
-    Ok.chunked(sseConfiguration.sseSource)
+    Ok.chunked(sseService.getSseSource())
       .as(ContentTypes.EVENT_STREAM)
       .withHeaders("Cache-Control" -> "no-cache")
       .withHeaders("Connection" -> "keep-alive")
--- x-mentor-core/app/models/configurations/SSEConfiguration.scala
+++
@@ -1,8 +0,0 @@
-package models.configurations
-
-import akka.NotUsed
-import akka.actor.ActorRef
-import akka.stream.scaladsl.Source
-import play.api.libs.EventSource.Event
-
-case class SSEConfiguration(notificationActor: ActorRef, sseSource: Source[Event, NotUsed])
